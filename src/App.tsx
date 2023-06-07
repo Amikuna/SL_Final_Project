ec41dd68f6adf124cfb8075d8a40ef82fed6da98
kiu_makharadze.amirani <makharadze.amirani@kiu.edu.ge>
2023-06-07 17:43:22 +0400

    test1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste atque
+        autem id voluptas doloremque at libero? Voluptatem nam ducimus
+        reprehenderit, vero fugit, nostrum asperiores quam omnis quaerat optio,
+        eius assumenda?
+      </p>
+    </div>
+  );
 }
 
 export default App;
